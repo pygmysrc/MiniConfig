424222bfd63d0a00746634baa5bb41a1be7ca47b
pygmyux <hi-scores@yandex.com>
2023-06-30 22:51:19 -0300

    enable mini.starter

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,6 +3,22 @@ return {
   version = '*',
   lazy = false,
   config = function()
+    local starter = require 'mini.starter'
+    starter.setup {
+      evaluate_single = true,
+      items = {
+        starter.sections.builtin_actions(),
+        starter.sections.recent_files(5, false),
+        starter.sections.recent_files(5, true),
+        -- Use this if you set up 'mini.sessions'
+        starter.sections.sessions(5, true),
+      },
+      content_hooks = {
+        starter.gen_hook.adding_bullet(),
+        starter.gen_hook.indexing('all', { 'Builtin actions' }),
+        starter.gen_hook.padding(3, 2),
+      }
+    }
     require('mini.bufremove').setup {}
     require('mini.indentscope').setup {}
     require('mini.tabline').setup {
